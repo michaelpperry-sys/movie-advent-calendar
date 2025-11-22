a03b4dbef03c673d3cbecddd323378d0699cd671
michaelpperry-sys <michaelpperry@gmail.com>
2025-11-22 12:41:02 -0500

    TMDB Key Update

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ async function fetchPosterUrl(tmdbId) {
 
     try {
         const response = await fetch(
-            `https://api.themoviedb.org/3/movie/${tmdbId}?api_key=${TMDB_API_KEY}`
+            `https://api.themoviedb.org/3/movie/${tmdbId}?api_key=${4aee5b1e27484ae41ca1d919dc1c203f}`
         );
         const data = await response.json();
 
